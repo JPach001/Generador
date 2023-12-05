15c4038c126436e85ef63d0c1f67567245aa6487
JPach001 <julianpacheco01@hotmail.com>
2023-12-05 11:43:07 -0600

    "Implementacion Or y else"

--- Lenguaje.cs
+++ Lenguaje.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace Generador
 {
+    
     public class Lenguaje : Sintaxis
     {
+        public int contEpsilon = 0;
+        public int contOr = 0;
         public Lenguaje()
         {
 
@@ -45,6 +48,8 @@ namespace Generador
         }
         private void Producciones()
         {
+            contEpsilon = 0;
+            contOr = 0;
             generado.WriteLine("        public void " + getContenido() + "()");
             generado.WriteLine("        {");
             match(Tipos.SNT);
@@ -59,22 +64,104 @@ namespace Generador
         }
         private void listaSimbolos()
         {
-            if (esPalabraReservada(getContenido()))
+            if(contEpsilon > 0)
             {
-                generado.WriteLine("            match(Tipos." + getContenido() + ");");
-                match(Tipos.SNT);
+                if (esPalabraReservada(getContenido()))
+                {
+                    generado.WriteLine("            match(Tipos." + getContenido() + ");");
+                    match(Tipos.SNT);
+                }
+                else if (getClasificacion() == Tipos.ST)
+                {
+                    generado.WriteLine("            match(\"" + getContenido() + "\");");
+                    match(Tipos.ST);
+                }
+                else if (getClasificacion() == Tipos.SNT)
+                {
+                    generado.WriteLine("            " + getContenido() + "();");
+                    match(Tipos.SNT);
+                }
+                if (getClasificacion() != Tipos.PDer)
+                {
+                    listaSimbolos();
+                }
+                else if (getClasificacion() == Tipos.PDer)
+                {
+                    match(Tipos.PDer);
+                    generado.WriteLine("            }");
+                    contEpsilon = 0;
+                }
             }
-            else if (getClasificacion() == Tipos.ST)
+            if (contOr > 0)
             {
-                generado.WriteLine("            match(\"" + getContenido() + "\");");
-                match(Tipos.ST);
+                string simbolo = getContenido();
+                generado.Write("            else ");
+                if (esPalabraReservada(getContenido()))
+                {
+                    match(Tipos.SNT);
+                    if(getClasificacion() != Tipos.PDer)
+                    {
+                        generado.Write("if (getClasificacion() == Tipos." + simbolo + ")");
+                    }
+                    generado.WriteLine("            {");
+                    generado.WriteLine("                match(Tipos." + simbolo + ");");
+                    generado.WriteLine("            }");
+                    
+                }
+                else if (getClasificacion() == Tipos.ST)
+                {
+                    match(Tipos.ST);
+                    if(getClasificacion() != Tipos.PDer)
+                    {
+                        generado.WriteLine("if (getContenido() == \"" + simbolo + "\")");
+                    }
+                    generado.WriteLine("            {");
+                    generado.WriteLine("                match(\"" + simbolo + "\");");
+                    generado.WriteLine("            }");
+                    
+                }
+                else if (getClasificacion() == Tipos.SNT)
+                {
+                    
+                    match(Tipos.SNT);
+                    generado.WriteLine("");
+                    generado.WriteLine("            {");
+                    generado.WriteLine("                " + simbolo + "();");
+                    generado.WriteLine("            }");
+                    if(getClasificacion() != Tipos.PDer){
+                        throw new Error("No se puede esperar otra condicion debido a que ya hay un else", log, linea, columna);
+                    }
+                }
+                if(getClasificacion() != Tipos.PDer)
+                {
+                    listaSimbolos();
+                }
+                else if(getClasificacion() == Tipos.PDer)
+                {
+                    match(Tipos.PDer);
+                    contOr = 0;
+                }
             }
-            else if (getClasificacion() == Tipos.SNT)
+            else
             {
-                generado.WriteLine("            " + getContenido() + "();");
-                match(Tipos.SNT);
+                if (esPalabraReservada(getContenido()))
+                {
+                    generado.WriteLine("            match(Tipos." + getContenido() + ");");
+                    match(Tipos.SNT);
+                }
+                else if (getClasificacion() == Tipos.ST)
+                {
+                    generado.WriteLine("            match(\"" + getContenido() + "\");");
+                    match(Tipos.ST);
+                }
+                else if (getClasificacion() == Tipos.SNT)
+                {
+                    generado.WriteLine("            " + getContenido() + "();");
+                    match(Tipos.SNT);
+                }
             }
-            else if (getClasificacion() == Tipos.Epsilon)
+            /*----------*/
+            if (getClasificacion() == Tipos.Epsilon)
             {
                 match(Tipos.Epsilon);
                 match(Tipos.PIzq);
@@ -85,7 +172,6 @@ namespace Generador
                     generado.WriteLine("            if (getClasificacion() == Tipos."+simbolo+")");
                     generado.WriteLine("            {");
                     generado.WriteLine("                match(Tipos." + simbolo + ");");
-                    Epsilon();
                 }
                 else if (getClasificacion() == Tipos.ST)
                 {
@@ -93,18 +179,47 @@ namespace Generador
                     match(Tipos.ST);
                     generado.WriteLine("            {");
                     generado.WriteLine("                match(\"" + simbolo + "\");");
-                    Epsilon();
                 }
                 else
                 {
-                    throw new Error("de sintaxis, debe <" + getContenido() + "> debe ser un ST o Palabra Reservada", log, linea, columna);
+                    //throw new Error("de sintaxis, debe <" + getContenido() + "> debe ser un ST o Palabra Reservada", log, linea, columna);
                 }
-                generado.WriteLine("            }");
-                match(Tipos.PDer);
+                if (getClasificacion() != Tipos.PDer)
+                {
+                    contEpsilon++;
+                    listaSimbolos();
+                }
+                /*generado.WriteLine("            }");
+                match(Tipos.PDer);*/
             }
             else if (getClasificacion() == Tipos.Or)
             {
-                
+                match(Tipos.Or);
+                match(Tipos.PIzq);
+                string simbolo = getContenido();
+                if (esPalabraReservada(simbolo))
+                {
+                    match(Tipos.SNT);
+                    generado.WriteLine("            if (getClasificacion() == Tipos." + simbolo + ")");
+                    generado.WriteLine("            {");
+                    generado.WriteLine("                match(Tipos." + simbolo + ");");
+                }
+                else if(getClasificacion() == Tipos.ST)
+                {
+                    generado.WriteLine("            if (getContenido() == \"" + simbolo + "\")");
+                    match(Tipos.ST);
+                    generado.WriteLine("            {");
+                    generado.WriteLine("                match(\"" + simbolo + "\");");
+                }
+                else {
+                    throw new Error("No se puede iniciar un Epsilon con un SNT", log, linea, columna);
+                }
+                generado.WriteLine("            }");
+                if (getClasificacion() != Tipos.PDer)
+                {
+                    contOr++;
+                    listaSimbolos();
+                }
             }
             if (getClasificacion() != Tipos.FinProduccion)
             {
@@ -138,37 +253,6 @@ namespace Generador
                     return true;
             }
             return false;
-        }
-        /*
-            
-        */
-        private void Epsilon()
-        {
-            if (esPalabraReservada(getContenido()))
-            {
-                generado.WriteLine("                match(Tipos." + getContenido() + ");");
-                match(Tipos.SNT);
-            }
-            else if (getClasificacion() == Tipos.ST)
-            {
-                generado.WriteLine("                match(\"" + getContenido() + "\");");
-                match(Tipos.ST);
-            }
-            else if (getClasificacion() == Tipos.SNT)
-            {
-                generado.WriteLine("                " + getContenido() + "();");
-                match(Tipos.SNT);
-            }
-            if (getClasificacion() != Tipos.PDer)
-                Epsilon();
-        }
-        private void Or()
-        {
-            string NextSimbolo = getContenido();
-            if(getClasificacion() != Tipos.PDer)
-            {
-                
-            }
         }
     }
 }
